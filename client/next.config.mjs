4fac85b56d24e765b9d905c72656985dbe748fd6
Tom Biato <biatotom@gmail.com>
2024-11-27 17:09:38 +0100

    Modify S3 config

--- client/next.config.mjs
+++ client/next.config.mjs
@@ -4,7 +4,7 @@ const nextConfig = {
     remotePatterns: [
       {
         protocol: "https",
-        hostname: "s3-inventorymanagement.s3.us-east-2.amazonaws.com",
+        hostname: "s3-inventorymanagement.s3.eu-west-3.amazonaws.com",
         port: "",
         pathname: "/**",
       },
